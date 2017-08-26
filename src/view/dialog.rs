a6c1c67ffa7074254810438deb7e75bc29b41de9
hcpl <hcpl.prog@gmail.com>
2017-08-26 16:29:11 +0300

    Integrate command handling

--- src/view/dialog.rs
+++ src/view/dialog.rs
@@ -1,20 +1,24 @@
 use cursive::align::HAlign;
-use cursive::event::{Event, Key};
-use cursive::view::{Identifiable, ViewWrapper};
+use cursive::direction::Direction;
+use cursive::event::{Event, EventResult, Key};
+use cursive::view::{Finder, Identifiable, View, ViewWrapper};
 use cursive::views::{BoxView, IdView, LinearLayout, OnEventView, TextArea, TextView};
 
 use common::Action;
+use commands::{Command, CommandImpl, parse_command};
 use cursive_views::messages_view::MessagesView;
 use error;
+use mode::Mode;
 use utils;
 
 
 pub struct Dialog {
     layout: LinearLayout,
+    mode: Mode,
 }
 
 impl Dialog {
-    pub fn new() -> error::Result<Dialog> {
+    pub fn new() -> error::Result<IdView<OnEventView<Dialog>>> {
         let layout = LinearLayout::vertical()
             .child(Dialog::create_dialog_title())
             .child(Dialog::create_messages_display_area())
@@ -22,9 +26,63 @@ impl Dialog {
             .child(Dialog::create_message_edit_area())
             .child(Dialog::create_command_field());
 
-        Ok(Dialog {
+        let dialog = Dialog {
             layout: layout,
-        })
+            mode: Mode::Normal,
+        };
+
+        let evented_dialog = OnEventView::new(dialog)
+            .on_pre_event_inner(Event::Char(':'), |dialog| {
+                match dialog.mode {
+                    Mode::Normal => {
+                        dialog.find_id("command-field", |field: &mut IdView<OnEventView<TextArea>>| {
+                            field.get_mut().with_view_mut(|f| {
+                                f.on_event(Event::Char(':'));
+                                f.take_focus(Direction::none());
+                            })
+                        });
+                        dialog.mode = Mode::CommandLine;
+
+                        Some(EventResult::Consumed(None))
+                    },
+                    _ => None,
+                }
+            })
+            .on_pre_event_inner(Event::Char('i'), |dialog| {
+                match dialog.mode {
+                    Mode::Normal => {
+                        dialog.mode = Mode::Insert;
+                        Some(EventResult::Consumed(None))
+                    },
+                    Mode::Insert => {
+                        dialog.find_id("message-edit", |edit: &mut IdView<OnEventView<TextArea>>| {
+                            edit.on_event(Event::Char('i'));
+                        });
+                        Some(EventResult::Consumed(None))
+                    },
+                    Mode::CommandLine => {
+                        dialog.find_id("command-field", |edit: &mut IdView<OnEventView<TextArea>>| {
+                            edit.on_event(Event::Char('i'));
+                        });
+                        Some(EventResult::Consumed(None))
+                    },
+                }
+            })
+            .on_pre_event_inner(Event::Key(Key::Esc), |dialog| {
+                match dialog.mode {
+                    Mode::Normal => None,
+                    Mode::Insert => {
+                        dialog.mode = Mode::Normal;
+                        Some(EventResult::Consumed(None))
+                    },
+                    Mode::CommandLine => {
+                        dialog.mode = Mode::Normal;
+                        Some(EventResult::Consumed(None))
+                    },
+                }
+            });
+
+        Ok(evented_dialog.with_id("dialog"))
     }
 
     fn create_dialog_title() -> IdView<TextView> {
@@ -36,6 +94,7 @@ impl Dialog {
     }
 
     fn create_messages_display_area() -> BoxView<IdView<MessagesView>> {
+        // TODO: fetch real messages
         BoxView::with_full_screen(MessagesView::new()
             .action(Action::online(
                 "foo",
@@ -71,30 +130,43 @@ impl Dialog {
         let prompt = "prompt";
         let initial_message_text = "message text";
 
-        let message_edit_area = LinearLayout::horizontal()
-            .child(TextView::new(prompt))
-            .child(BoxView::with_full_width(
-                OnEventView::new(TextArea::new().content(initial_message_text).with_id("message-edit"))
-                    .on_event(Event::Ctrl(Key::Enter), |s| {
-                        s.call_on_id("message-edit", |v: &mut IdView<TextArea>| {
-                            v.get_mut().set_content("");
-                        });
-                    })));
+        let message_edit_area = OnEventView::new(TextArea::new().content(initial_message_text))
+            .with_id("message-edit");
 
-        message_edit_area
+        let message_edit_area_wrapper = LinearLayout::horizontal()
+            .child(TextView::new(prompt))
+            .child(BoxView::with_full_width(message_edit_area));
+
+        message_edit_area_wrapper
     }
 
-    fn create_command_field() -> OnEventView<IdView<TextArea>> {
-        let command_field = OnEventView::new(TextArea::new().with_id("command-field"))
-            .on_event(Event::Char(':'), |s| {
-                /*s.call_on_id("dialog-state", |v: &mut IdView<DialogState>| {
-                    v.get_mut().set_state(Mode::CommandLine);
-                });*/
+    fn create_command_field() -> IdView<OnEventView<TextArea>> {
+        let command_field = OnEventView::new(TextArea::new())
+            .on_pre_event_inner(Event::Key(Key::Backspace), |v| {
+                if v.get_content() == ":" {
+                    v.find_id("dialog", |d: &mut OnEventView<Dialog>| {
+                        d.with_view_mut(|d| {
+                            d.mode = Mode::Normal;
+                        });
+                    });
+                }
 
-                s.call_on_id("command-field", |v: &mut IdView<TextArea>| {
-                    v.get_mut().set_content(":");
-                });
-            });
+                v.on_event(Event::Key(Key::Backspace));
+
+                Some(EventResult::Consumed(None))
+            })
+            .on_pre_event_inner(Event::Key(Key::Enter), |v| {
+                let command: CommandImpl = parse_command(v.get_content()).unwrap();
+                command.execute().unwrap();
+
+                // Clearing
+                while v.get_content() != "" {
+                    v.on_event(Event::Key(Key::Backspace));
+                }
+
+                Some(EventResult::Consumed(None))
+            })
+            .with_id("command-field");
 
         command_field
     }
