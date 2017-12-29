dad9bf80eba7bc2dc96bc8610b35bf78bd4900db
hcpl <hcpl.prog@gmail.com>
2017-12-29 03:27:54 +0200

    Adapt to new language features
    Such as `compile_error!` macro from Rust 1.20.
    
    Also add Travis CI support for use in the future.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate clap;
 extern crate config;
 #[macro_use]
 extern crate cursive;
+extern crate env_logger;
 #[macro_use]
 extern crate error_chain;
 #[macro_use]
@@ -21,6 +22,35 @@ extern crate structopt_derive;
 extern crate textwrap;
 
 
+#[cfg(not(any(
+    all(feature = "blt-backend",
+        not(feature = "ncurses-backend"),
+        not(feature = "pancurses-backend"),
+        not(feature = "termion-backend")),
+
+    all(not(feature = "blt-backend"),
+        feature = "ncurses-backend",
+        not(feature = "pancurses-backend"),
+        not(feature = "termion-backend")),
+
+    all(not(feature = "blt-backend"),
+        not(feature = "ncurses-backend"),
+        feature = "pancurses-backend",
+        not(feature = "termion-backend")),
+
+    all(not(feature = "blt-backend"),
+        not(feature = "ncurses-backend"),
+        not(feature = "pancurses-backend"),
+        feature = "termion-backend"),
+)))]
+compile_error!("Exactly one backend must be used with this crate. \
+    Please specify either of:
+        `features = [\"blt-backend\"]`
+        `features = [\"ncurses-backend\"]`
+        `features = [\"pancurses-backend\"]`
+        `features = [\"termion-backend\"]`");
+
+
 mod action;
 mod app_config;
 mod args;
@@ -35,6 +65,8 @@ mod view;
 
 
 fn run() -> error::Result<()> {
+    env_logger::init();
+
     let mut app_config = args::process_args()?;
     let mut siv = view::create_cursive_session(&mut app_config)?;
 
